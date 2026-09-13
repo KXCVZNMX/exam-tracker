51cf0ef331b9e8494a9cd756ce46ff3ce6b7e9d1
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-09-13 23:44:23 +1000

    feat: types for subjects (used for db storage and typed fetching)

--- src/lib/types/subjects.ts
+++ src/lib/types/subjects.ts
@@ -2,5 +2,24 @@ export interface SubjectsContent {
 	userId: string;
 	name: string;
 	description?: string;
+	numSections: number;
 	createdAt: Date;
+}
+
+export interface SubjectExams {
+	userId: string;
+	subjectId: string;
+	examName: string;
+	exams: Exam[];
+}
+
+export interface Exam {
+	company: string;
+	year: number;
+	completed: boolean;
+	partAMark: number;
+	partBMark: number;
+	partAFullMark: number;
+	partBFullMark: number;
+	comments: string[];
 }
