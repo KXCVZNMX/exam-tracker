e283874106efba79a65207a1affd85577c4127dc
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-09-13 23:59:28 +1000

    feat: wip subject page

---
+++ src/routes/(main)/subjects/[subjectId]/+page.server.ts
@@ -0,0 +1,37 @@
+import type {PageServerLoad} from './$types'
+import db from '$lib/server/mongodb';
+import type { SubjectExams, SubjectsContent } from '$lib/types/subjects';
+import { error } from '@sveltejs/kit';
+import { ObjectId } from 'mongodb';
+
+export const load: PageServerLoad = async ({ params, locals }) => {
+	const session = locals.session;
+	if (!session || !locals.user) {
+		throw error(403, 'Forbidden');
+	}
+
+	let subjectExams;
+	let subject;
+	try {
+		subject = await db.collection<SubjectsContent>('subjects').findOne({
+			_id: new ObjectId(params.subjectId),
+			userId: session.userId,
+		});
+		subjectExams = await db.collection<SubjectExams>('subject-exams').findOne({
+			subjectId: params.subjectId,
+			userId: session.userId
+		});
+	} catch {
+		throw error(400, 'Invalid subject id');
+	}
+
+	if (!subject) {
+		throw error(404, 'Subject not found');
+	}
+
+	return {
+		subject: subject ? { ...subject, _id: subject._id.toString() } : null,
+
+		subjectExams: subjectExams ? { ...subjectExams, _id: subjectExams._id.toString() } : null
+	};
+}
---
+++ src/routes/(main)/subjects/[subjectId]/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import {Plus, Check} from "@lucide/svelte";
+
+    let { data }: { data: PageData } = $props();
+</script>
+
+<div class="p-5">
+    <h1 class="pt-10 text-5xl font-bold">{data.subject.name}</h1>
+
+    <div class="mb-4 flex justify-end">
+        <button
+                class="md:rounded-btn btn aspect-square rounded-full px-0 btn-md btn-primary md:aspect-auto md:px-4"
+                aria-label="Add Exams"
+        >
+            <Plus />
+            <span class="hidden md:inline">Add Exams</span>
+        </button>
+    </div>
+
+    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="w-[15%]">Company</th>
+                    <th class="w-[10%]">Year</th>
+                    <th class="w-[5%]">Done</th>
+
+                    <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
+                    {#each { length: data.subject.numSections } as _, i (i)}
+                        <th style={`width: calc(30% / ${data.subject.numSections})`}>
+                            Section {i + 1}
+                        </th>
+                    {/each}
+
+                    <th class="w-[40%]">Comments</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>MAV</td>
+                    <td>2025</td>
+                    <td><Check /></td>
+                    <td>38/40</td>
+                    <td>76/80</td>
+                    <td>Nothing Yet</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
