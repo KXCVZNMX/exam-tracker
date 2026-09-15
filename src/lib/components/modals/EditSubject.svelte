eabc6c15aefd00e17752892e8c5f785acf521373
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-09-15 19:12:00 +1000

    feat: added danger zone for deleting subject

--- src/lib/components/modals/EditSubject.svelte
+++ src/lib/components/modals/EditSubject.svelte
@@ -16,6 +16,7 @@
 		subjectId: string;
 	} = $props();
 	let submitting = $state(false);
+	let deleting = $state(false);
 </script>
 
 <dialog class="modal" class:modal-open={show}>
@@ -74,26 +75,59 @@
 					rows="3"> {description} </textarea>
 			</fieldset>
 
-			<fieldset class="fieldset w-full sm:w-1/2">
-				<legend class="fieldset-legend">Number of Sections</legend>
-				<input
-					type="number"
-					name="sections"
-					class="input w-full rounded-sm"
-					value={numSections}
-					min="1"
-					max="4"
-					required
-				/>
-			</fieldset>
+			<div class="flex flex-col sm:flex-row items-stretch sm:items-end gap-4">
+				<fieldset class="fieldset w-full sm:flex-1">
+					<legend class="fieldset-legend">Number of Sections</legend>
+					<input
+							type="number"
+							name="sections"
+							class="input w-full rounded-sm"
+							value={numSections}
+							min="1"
+							max="4"
+							required
+					/>
+				</fieldset>
 
-			<div class="flex justify-end gap-2 pt-2">
-				<button type="button" class="btn btn-ghost" onclick={() => (show = false)}> Cancel </button>
+				<div class="flex gap-2 pb-0 sm:pb-1">
+					<button
+							type="button"
+							class="btn btn-ghost"
+							onclick={() => (show = false)}
+					>
+						Cancel
+					</button>
 
-				<button type="submit" class="btn btn-primary" disabled={submitting}>
-					{submitting ? 'Saving...' : 'Save Subject'}
-				</button>
+					<button type="submit" class="btn btn-primary" disabled={submitting}>
+						{submitting ? 'Saving...' : 'Save Subject'}
+					</button>
+				</div>
 			</div>
+		</form>
+
+		<form
+			method="POST"
+			action="?/deleteSubject"
+			class="mt-6"
+			use:enhance={() => {
+				deleting = true;
+				return async ({ result, update }) => {
+					deleting = false;
+					if (result.type === 'success') show = false;
+					await update();
+				};
+			}}
+		>
+			<input type="hidden" name="id" value={subjectId} />
+			<fieldset class="fieldset rounded-box border border-error p-4">
+				<legend class="fieldset-legend text-error">Danger zone</legend>
+				<p class="text-sm">
+					Permanently delete this subject and all of its exams. This cannot be undone.
+				</p>
+				<button type="submit" class="btn mt-2 btn-error" disabled={submitting || deleting}>
+					{deleting ? 'Deleting...' : 'Delete Subject'}
+				</button>
+			</fieldset>
 		</form>
 	</div>
 </dialog>
