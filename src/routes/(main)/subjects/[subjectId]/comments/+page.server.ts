56629e800dd4384489154b1c4ed6077200ec5ecb
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-09-17 17:16:19 +1000

    feat: comments load function

--- src/routes/(main)/subjects/[subjectId]/comments/+page.server.ts
+++ src/routes/(main)/subjects/[subjectId]/comments/+page.server.ts
@@ -1,5 +1,66 @@
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import db from '$lib/server/mongodb';
+import type { Comments, Exam, SubjectsContent } from '$lib/types/subjects';
+import { ObjectId } from 'mongodb';
+
+type CommentsWithName = {
+	company: string;
+	year: number;
+	comments: Comments[];
+}
 
 export const load: PageServerLoad = async ({ params, locals }) => {
+	const session = locals.session;
+	if (!session || !locals.user) {
+		throw error(403, 'Forbidden');
+	}
 
+	let subjectName;
+	let subjectContent: Exam[];
+	try {
+		subjectName = await db.collection<SubjectsContent>('subjects').findOne(
+			{
+				_id: new ObjectId(params.subjectId),
+				userId: session.userId,
+			},
+			{
+				projection: {
+					name: 1
+				}
+			}
+		);
+
+		subjectContent = await db.collection<Exam>('exams').find(
+			{
+				subjectId: params.subjectId,
+				userId: session.userId,
+			},
+			{
+				projection: {
+					company: 1,
+					year: 1,
+					comments: 1
+				}
+			}
+		).toArray();
+	} catch {
+		throw error(400, 'Invalid subject ID');
+	}
+
+	const subjectComments: CommentsWithName[] = subjectContent
+		.map((exam) => ({
+			company: exam.company,
+			year: exam.year,
+			comments: exam.comments,
+		}));
+
+	if (!subjectName) {
+		throw error(404, 'Subject Not Found');
+	}
+
+	return {
+		subjectName: subjectName.name,
+		subjectComments,
+	};
 }
