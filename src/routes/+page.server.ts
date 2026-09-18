438908573bdcc1994f3c7f582e0ed6ac28fcb1fe
Kevin Chen <CC6391@scotchmel.vic.edu.au>
2026-09-18 14:13:12 +1000

    fix: missing addSubject endpoint in root/+page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,39 @@
 import type { PageServerLoad } from './$types';
+import { type Actions, error } from '@sveltejs/kit';
+import db from '$lib/server/mongodb';
+import type { SubjectsContent } from '$lib/types/subjects';
+
+export const actions: Actions = {
+	addSubject: async ({ request, locals }) => {
+		const data = await request.formData();
+		const name = data.get('name')?.toString().trim();
+		const description = data.get('description')?.toString().trim() || '';
+		const numSections = data.get('sections')?.toString().trim();
+
+		const session = locals.session;
+		if (!session || !locals.user) {
+			throw error(403, 'Forbidden');
+		}
+
+		if (!name || !numSections) {
+			throw error(400, 'Subject name and sections are required');
+		}
+
+		const numSectionsParsed = parseInt(numSections, 10);
+
+		if (Number.isNaN(numSectionsParsed) || numSectionsParsed > 4 || numSectionsParsed < 1) {
+			throw error(403, 'Subject Sections should be between 1 to 4 and not NaN');
+		}
+
+		await db.collection<SubjectsContent>('subjects').insertOne({
+			userId: session.userId,
+			name,
+			description,
+			numSections: parseInt(numSections, 10),
+			createdAt: new Date()
+		});
+	}
+};
 
 export const load: PageServerLoad = async ({ locals }) => {
 	return {
